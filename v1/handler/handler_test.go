4081a69b7ce93e933484e8b1dc865fd7a665f2c3
muazwzxv <muazwazir@gmail.com>
2022-04-16 18:14:52 +0800

    getAccountById test

--- v1/handler/handler_test.go
+++ v1/handler/handler_test.go
@@ -2,12 +2,49 @@ package handler
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"testing"
 )
+
+func TestGetAccountById(t *testing.T) {
+	tests := []struct {
+		description  string
+		route        string
+		method       string
+		expectedCode int
+		id           int
+	}{
+		{
+			description:  "Get All Accounts status 200",
+			route:        fmt.Sprintf("http://localhost:8080/api/account/%d", 3),
+			method:       "GET",
+			expectedCode: fiber.StatusOK,
+			id:           3,
+		},
+	}
+
+	for _, test := range tests {
+		req, err := http.NewRequest(test.method, test.route, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		data, _ := ioutil.ReadAll(resp.Body)
+		t.Logf("%s", data)
+
+		assert.Equal(t, test.expectedCode, resp.StatusCode)
+	}
+}
 
 func TestGetAllAccount(t *testing.T) {
 	tests := []struct {
